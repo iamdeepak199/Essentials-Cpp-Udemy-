639a91e8365cee497fde212d0504a9385ed68600
Deepak bhardwaj <80276722+iamdeepak199@users.noreply.github.com>
2025-12-19 17:16:41 +0530

    Delete Node from Front & Back : In Linkedlist

---
+++ Linkedlist/DeleteFromBack.cc
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define endl "\n"
+
+void fastio(bool read = false) {
+    if (read) {
+        #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
+        #endif
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+// Node class
+class node {
+public:
+    int data;
+    node* next;
+
+    node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node*& head, int val) {
+    node* new_node = new node(val);
+    new_node->next = head;
+    head = new_node;
+}
+
+void deleteAtTail(node*& head) {
+    if (head == NULL) {
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }       
+    temp->next = NULL;
+}
+void display(node* head) {
+    node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    fastio(true);
+
+    node* head = NULL;
+
+    cout << "Enter number of nodes: ";
+    int n;
+    cin >> n;
+
+    cout << "Enter " << n << " values:" << endl;
+    for (int i = 1; i <= n; i++) {
+        int val;
+        cin >> val;
+        insertAtHead(head, val);
+    }
+
+    cout << "Linked list: Before delete" << endl;
+    display(head);
+
+    deleteAtTail(head);  
+
+    cout << "Linked list: After delete" << endl;
+    display(head);
+
+    return 0;
+}
+
+/*
+
+output
+
+Enter number of nodes: Enter 6 values:
+Linked list: Before delete
+6 5 4 3 2 1 
+Linked list: After delete
+6 5 4 3 2 
+
+*/
